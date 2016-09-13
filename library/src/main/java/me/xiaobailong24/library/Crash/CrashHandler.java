932dda7b350fe454f7c79f2007fcf1abf9929b44
xiaobailong24 <xiaobailong24@gmail.com>
2016-09-13 10:00:14 +0800

    v1.0.4 Add CrashHandler

---
+++ library/src/main/java/me/xiaobailong24/library/Crash/CrashHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016.  @xiaobailong24
+ */
+
+package me.xiaobailong24.library.Crash;
+
+/**
+ * Created by LiuYinlong on 2016/9/13.
+ * 自定义的 异常处理类 , 实现了 UncaughtExceptionHandler接口
+ */
+
+import android.content.Context;
+
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+    // 需求是 整个应用程序 只有一个 MyCrash-Handler
+    private static CrashHandler INSTANCE ;
+    private Context context;
+
+    //1.私有化构造方法
+    private CrashHandler(){
+
+    }
+
+    public static synchronized CrashHandler getInstance(){
+        if (INSTANCE == null)
+            INSTANCE = new CrashHandler();
+        return INSTANCE;
+    }
+
+    public void init(Context context){
+        this.context = context;
+    }
+
+
+    public void uncaughtException(Thread arg0, Throwable arg1) {
+        System.out.println("程序挂掉了 ");
+        // 在此可以把用户手机的一些信息以及异常信息捕获并上传,由于UMeng在这方面有很程序的api接口来调用，故没有考虑
+
+        //干掉当前的程序
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+}
